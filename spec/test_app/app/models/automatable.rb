1039093e75abdc60ff553acdc68994707a2edbd5
Rahoul Baruah <rahoulb@echodek.co>
2025-09-25 17:18:13 +0100

    Lint

--- spec/test_app/app/models/automatable.rb
+++ spec/test_app/app/models/automatable.rb
@@ -1,5 +1,6 @@
 class Automatable < ApplicationRecord
   include Automations::Container
+
   has_many :folders, foreign_key: "project_id", dependent: :destroy
   validates :name, presence: true
 end
